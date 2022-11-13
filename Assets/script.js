6ca152c49cd7fcd6f90aef4e8e1a681a6d0a4f11
alexisbenavidez99 <callmealexis49@gmail.com>
2022-11-12 18:16:14 -0600

    added link/details to README and updated js

--- Assets/script.js
+++ Assets/script.js
@@ -1,2 +1,31 @@
-var today = dayjs().format('dddd, MMM D');
-$('#currentDay').text(today);
+var today = dayjs();
+$('#currentDay').text(today.format('dddd, MMMM D'));
+
+var timeBlock = $('.description');
+
+var pastTime = $('<div>');
+var presentTime = $('<div>');
+var futureTime = $('<div>');
+
+pastTime.attr('class', 'past');
+presentTime.attr('class', 'present');
+futureTime.attr('class', 'future');
+
+function getTime () {
+    var currentHour = dayjs().get('h');
+
+    for (i = 9; i < 18; i++) {
+        if (currentHour === i) {
+            currentHour = presentTime;
+            timeBlock.style = presentTime
+        } else if (currentHour > i) {
+            currentHour = pastTime;
+            timeBlock.style = pastTime;
+        } else {
+            currentHour = futureTime;
+            timeBlock.style = futureTime;
+        }
+    }
+}
+
+
